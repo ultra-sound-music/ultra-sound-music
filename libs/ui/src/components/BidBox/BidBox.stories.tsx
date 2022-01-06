e2724a46dbb78663907bdb11492c2c89b421a8b7
Vijay Rudraraju <vijay326@gmail.com>
2022-01-05 21:23:27 -0800

    Add initial BidBox component + story

---
+++ libs/ui/src/components/BidBox/BidBox.stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from '@storybook/react';
+import { BidBox, BidBoxProps } from './BidBox';
+
+export default {
+  component: BidBox,
+  title: 'Components/BidBox',
+} as Meta;
+
+const Template: Story<BidBoxProps> = (args) => <BidBox {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
---
+++ libs/ui/src/components/BidBox/BidBox.tsx
@@ -0,0 +1,14 @@
+import './BidBox.module.scss';
+
+/* eslint-disable-next-line */
+export interface BidBoxProps {}
+
+export function BidBox(props: BidBoxProps) {
+  return (
+    <div>
+      <h1>Welcome to BidBox!</h1>
+    </div>
+  );
+}
+
+export default BidBox;
