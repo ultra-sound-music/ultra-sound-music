b66f215d9f0aa90587810701294a62f32760feb5
Vijay Rudraraju <vijay326@gmail.com>
2022-01-09 00:32:33 -0800

    More partial work on BidBox

--- libs/ui/src/components/BidBox/BidBox.tsx
+++ libs/ui/src/components/BidBox/BidBox.tsx
@@ -1,4 +1,5 @@
 import cn from 'clsx';
+import { Button } from '../Button/Button';
 
 import styles from './BidBox.scss';
 
@@ -13,10 +14,28 @@ export function BidBox(props: BidBoxProps): JSX.Element {
   return (
     <div className={classNames}>
       <div className={styles.info}>
-        <div className={styles.currentBid}></div>
-        <div className={styles.walletBalance}></div>
+        <div className={styles.currentBid}>
+          <p>Current Bid</p>
+          <div className={styles.currentBidValue}>
+            <h2>173.50 SOL</h2>
+          </div>
+        </div>
+        <div className={styles.walletBalance}>
+          <p>Connect a Wallet</p>
+          <div className={styles.walletBalanceValue}>
+            <h2>-</h2>
+          </div>
+        </div>
       </div>
-      <div className={styles.form}></div>
+      <div className={styles.form}>
+        <p>Connect a Wallet</p>
+        <div className={styles.formInputBox}>
+          <div className={styles.formInput}></div>
+          <Button isDisabled type="primary">
+            Connect
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
