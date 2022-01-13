b475cfde948b0593b4d50977241569793485880f
Gabriel <g@brielcastillo.me>
2022-01-13 12:07:17 -0800

    fix

--- libs/ui/src/components/Grid/Grid.tsx
+++ libs/ui/src/components/Grid/Grid.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import cn from 'clsx';
 
 import styles from './Grid.scss';
 
@@ -8,7 +9,7 @@ export interface IGridProps {
 }
 
 export function Grid({ className, children }: IGridProps) {
-  return <div className={`${styles.Grid} ${className}`}>{children}</div>;
+  return <div className={`${cn(styles.Grid, className)}`}>{children}</div>;
 }
 
 export default Grid;
