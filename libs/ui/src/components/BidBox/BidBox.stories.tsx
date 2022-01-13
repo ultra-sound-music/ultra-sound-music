6c4951f10d871d5c047cd260df288c50127204d4
Vijay Rudraraju <vijay326@gmail.com>
2022-01-13 00:36:44 -0800

    Update BidBox

--- libs/ui/src/components/BidBox/BidBox.stories.tsx
+++ libs/ui/src/components/BidBox/BidBox.stories.tsx
@@ -9,4 +9,24 @@ export default {
 const Template: Story<BidBoxProps> = (args) => <BidBox {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  timeUntilAuctionEnd: { days: 0, hours: 21, minutes: 46, seconds: 11 },
+  currentHighBidSol: 173.5,
+  isWalletConnected: true,
+  walletBalanceSol: 513.21,
+  recentBids: [
+    {
+      amountSol: 173.5,
+      userWalletAddress: '0x123',
+      timeSinceBid: { seconds: 19 }
+    },
+    {
+      amountSol: 101.0,
+      userWalletAddress: '0xvasd12asd3',
+      timeSinceBid: { minutes: 45 }
+    }
+  ],
+  isAuctionFinished: false,
+  traits: { name: 'Jam Bot #1' },
+  winningWalletAddress: '0x1ds...sdfsa'
+};
--- libs/ui/src/components/BidBox/BidBox.tsx
+++ libs/ui/src/components/BidBox/BidBox.tsx
@@ -5,14 +5,33 @@ import Paginate from '../Paginate/Paginate';
 import styles from './BidBox.scss';
 
 /* eslint-disable-next-line */
-export interface BidBoxProps {}
+export interface BidBoxProps {
+  timeUntilAuctionEnd: {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+  currentHighBidSol: number;
+  recentBids: {
+    amountSol: number;
+    userWalletAddress: string;
+    timeSinceBid: {
+      days?: number;
+      hours?: number;
+      minutes?: number;
+      seconds?: number;
+    };
+  }[];
+  isWalletConnected: boolean;
+  walletBalanceSol: number;
+  isAuctionFinished: boolean;
+  winningWalletAddress: string;
+  traits: { [key: string]: string };
+}
 
-export function BidBox(props: BidBoxProps): JSX.Element {
+export const BidBox = (props: BidBoxProps): JSX.Element => {
   console.log('BidBox: ', { props, styles });
-
-  const hours = 13;
-  const minutes = 27;
-  const seconds = 53;
 
   return (
     <div className={styles.mainContainer}>
@@ -24,7 +43,11 @@ export function BidBox(props: BidBoxProps): JSX.Element {
       </div>
       <div className={styles.timerContainer}>
         <p>
-          Auction ends in {hours}hours {minutes}minutes {seconds}seconds
+          Auction ends in {props.timeUntilAuctionEnd.days}
+          <strong>days</strong> {props.timeUntilAuctionEnd.hours}
+          <strong>hours</strong> {props.timeUntilAuctionEnd.minutes}
+          <strong>minutes</strong> {props.timeUntilAuctionEnd.seconds}
+          <strong>seconds</strong>
         </p>
       </div>
       <div className={styles.bidBoxContainer}>
@@ -32,44 +55,98 @@ export function BidBox(props: BidBoxProps): JSX.Element {
           <div className={styles.currentBid}>
             <p>Current Bid</p>
             <div className={styles.currentBidValue}>
-              <h2>173.50 SOL</h2>
+              <h2>{props.currentHighBidSol} SOL</h2>
             </div>
           </div>
-          <div className={styles.walletBalance}>
-            <p>Connect a Wallet</p>
-            <div className={styles.walletBalanceValue}>
-              <h2>-</h2>
+          {!props.isWalletConnected && !props.isAuctionFinished && (
+            <div className={styles.walletBalance}>
+              <p>Connect a Wallet</p>
+              <div className={styles.walletBalanceValue}>
+                <h2>-</h2>
+              </div>
+            </div>
+          )}
+          {props.isWalletConnected && !props.isAuctionFinished && (
+            <div className={styles.walletBalance}>
+              <p>In your wallet</p>
+              <div className={styles.walletBalanceValue}>
+                <h2>{props.walletBalanceSol} SOL</h2>
+              </div>
+            </div>
+          )}
+          {props.isAuctionFinished && (
+            <div className={styles.walletBalance}>
+              <p>Winner</p>
+              <div className={styles.walletBalanceValue}>
+                <h2>{props.winningWalletAddress}</h2>
+              </div>
+            </div>
+          )}
+        </div>
+        {!props.isWalletConnected && !props.isAuctionFinished && (
+          <div className={styles.form}>
+            <p>Your Bid</p>
+            <div className={styles.formInputBox}>
+              <div className={styles.formInput}>
+                <input
+                  type='number'
+                  placeholder='Bid more than 0.0 SOL'
+                  disabled
+                />
+                <span>SOL</span>
+              </div>
+              <Button isDisabled type='primary'>
+                Connect
+              </Button>
             </div>
           </div>
-        </div>
-        <div className={styles.form}>
-          <p>Connect a Wallet</p>
-          <div className={styles.formInputBox}>
-            <div className={styles.formInput}></div>
-            <Button isDisabled type='primary'>
-              Connect
-            </Button>
+        )}
+        {props.isWalletConnected && !props.isAuctionFinished && (
+          <div className={styles.form}>
+            <p>Your Bid</p>
+            <div className={styles.formInputBox}>
+              <div className={styles.formInput}>
+                <input type='number' placeholder='Bid more than 0.0 SOL' />
+                <span>SOL</span>
+              </div>
+              <Button type='primary'>Bid now</Button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <div className={styles.bidHistoryContainer}>
         <p>Bid history</p>
         <div className={styles.bidHistoryList}>
-          <div className={styles.bidHistoryItem}>
-            <div className={styles.bidAmount}>
-              <p>173.5 SOL</p>
-            </div>
-            <div className={styles.bidTime}>
-              <p>19 seconds ago</p>
-            </div>
-            <div className={styles.bidWalletAddress}>
-              <p>0x94..EBe0</p>
-            </div>
-          </div>
+          {props.recentBids.map(
+            ({ amountSol, userWalletAddress, timeSinceBid }) => (
+              <div className={styles.bidHistoryItem}>
+                <div className={styles.bidAmount}>
+                  <p>{amountSol} SOL</p>
+                </div>
+                <div className={styles.bidTime}>
+                  {timeSinceBid.seconds && timeSinceBid.seconds > 0 && (
+                    <p>{timeSinceBid.seconds} seconds ago</p>
+                  )}
+                  {timeSinceBid.minutes && timeSinceBid.minutes > 0 && (
+                    <p>{timeSinceBid.minutes} minutes ago</p>
+                  )}
+                  {timeSinceBid.hours && timeSinceBid.hours > 0 && (
+                    <p>{timeSinceBid.hours} hours ago</p>
+                  )}
+                  {timeSinceBid.days && timeSinceBid.days > 0 && (
+                    <p>{timeSinceBid.days} days ago</p>
+                  )}
+                </div>
+                <div className={styles.bidWalletAddress}>
+                  <p>{userWalletAddress}</p>
+                </div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default BidBox;
