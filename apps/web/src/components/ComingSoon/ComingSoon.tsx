8fb1d21ffcf60c5dfd9dc6a09b6c8eb8efa100d7
Gabriel <g@brielcastillo.me>
2022-02-01 13:25:00 -0800

    update callout

--- apps/web/src/components/ComingSoon/ComingSoon.tsx
+++ apps/web/src/components/ComingSoon/ComingSoon.tsx
@@ -13,16 +13,14 @@ export function ComingSoon() {
       <div className={styles.description}>
         <div className={styles.callout}>
           <Callout shout={false}>
-            Ultra Sound Music Jambots
-            <br /> <strong>Drop Coming Soon</strong>
+            <strong>NFT Drop Coming Soon<br /></strong>
+            Get updates and giveaways on <Link to={urls.usmTwitter}>Twitter</Link> and{' '} <Link to={urls.usmDiscord}>Discord</Link>
           </Callout>
           <div>
             <p>
               Ultra Sound Music is dedicated to creating composable, thematic
               music NFTs backed by real world musicians, and empowering real
-              world musicians. Our first drop are the Jam Bots. Get updates and
-              giveaways on <Link to={urls.usmTwitter}>Twitter</Link> and{' '}
-              <Link to={urls.usmDiscord}>Discord</Link>.
+              world musicians.
             </p>
           </div>
         </div>
