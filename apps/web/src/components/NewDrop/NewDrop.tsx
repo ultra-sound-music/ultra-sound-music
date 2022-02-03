e7ce441a0d8bad33bc8992dfb8fb8ee1ca9c39e7
Vijay Rudraraju <vijay326@gmail.com>
2022-02-02 22:03:56 -0800

    Update BidBox alignment within NewDrop

--- apps/web/src/components/NewDrop/NewDrop.tsx
+++ apps/web/src/components/NewDrop/NewDrop.tsx
@@ -2,11 +2,39 @@ import { BidBox, Grid } from '@usm/ui';
 
 import styles from './NewDrop.scss';
 
+// VIJX
 export function NewDrop() {
   return (
-    <Grid className={styles.NewDrop}>
-      <div className={styles.hero} />
-      <div className={styles.bidBox}>{/* <BidBox /> */}</div>
+    <Grid className={styles.Grid}>
+      <div className={styles.Avatar}></div>
+      <div className={styles.BidBox}>
+        <BidBox
+          timeUntilAuctionEnd={{
+            days: 0,
+            hours: 21,
+            minutes: 46,
+            seconds: 11
+          }}
+          currentHighBidSol={173.5}
+          isWalletConnected={true}
+          walletBalanceSol={512.12}
+          recentBids={[
+            {
+              amountSol: 173.5,
+              userWalletAddress: '0x123',
+              timeSinceBid: { seconds: 19 }
+            },
+            {
+              amountSol: 101.0,
+              userWalletAddress: '0xvasd12asd3',
+              timeSinceBid: { minutes: 45 }
+            }
+          ]}
+          isAuctionFinished={false}
+          winningWalletAddress='0x1ds...sdfsa'
+          traits={{ name: 'Jam Bot #1' }}
+        />
+      </div>
     </Grid>
   );
 }
