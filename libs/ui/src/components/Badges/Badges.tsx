b21ad4285420f13ff2d5e63a1308cdc580759f5e
Gabriel <g@brielcastillo.me>
2022-02-04 23:16:01 -0800

    ui updates

--- libs/ui/src/components/Badges/Badges.tsx
+++ libs/ui/src/components/Badges/Badges.tsx
@@ -1,15 +1,17 @@
-import debut from '@usm/assets/img/badges/debut.png'
-import hometown from '@usm/assets/img/badges/hometown.png'
-import version from '@usm/assets/img/badges/version.png'
+import Lottie from 'react-lottie-player';
 
-import Image from '../Image/Image';
+import debut from '@usm/assets/lottie/badges/debut.json';
+import hometown from '@usm/assets/lottie/badges/hometown.json';
+import version from '@usm/assets/lottie/badges/version.json';
 
 import styles from './Badges.scss';
 
-const badgesMap: Record<string, string> = {
+export type ILottieJson = Record<string, object> | undefined;
+
+const badgesMap: Record<string, object> = {
   debut,
   hometown,
-  version
+  version,
 }
 
 export interface IBadgesProps {
@@ -19,8 +21,12 @@ export interface IBadgesProps {
 export function Badges({ names }: IBadgesProps) {
   return (
     <div className={styles.Badges}>
-      {names.map((name: string) => {
-        return <div key={name} className={styles.badge}><Image src={badgesMap[name]} /></div>
+      {names?.map((name, i) => {
+        return (
+          <div key={i} className={styles.badge}>
+            <Lottie animationData={badgesMap[name]} direction={1} speed={0.5} play={true} loop={false} className={styles.lottie} />
+          </div>
+        )
       })}
     </div>
   );
--- libs/util/utils/src/index.ts
+++ libs/util/utils/src/index.ts
@@ -1,3 +1,7 @@
-export function createArray(size: number) {
+export function createNumberArray(size: number) {
   return Array.from(Array(size), (item, index) => index + 1);
+}
+
+export function createArray(size: number) {
+  return Array.from(Array(size));
 }
