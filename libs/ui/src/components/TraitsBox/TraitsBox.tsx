847c5960a03227406b6d4485e5da0d0854d83383
Vijay Rudraraju <vijay326@gmail.com>
2022-03-06 00:50:57 -0800

    TraitsBox layout

--- libs/ui/src/components/TraitsBox/TraitsBox.tsx
+++ libs/ui/src/components/TraitsBox/TraitsBox.tsx
@@ -7,10 +7,37 @@ export function TraitsBox(props: TraitsBoxProps) {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.left}>
-        <h1>Welcome to TraitsBox!</h1>
+        <div className={styles.top}>
+          <h1>Personal Traits</h1>
+          <div className={styles.personalTraits}>
+            <div>
+              <p>Sanity</p>
+            </div>
+            <div>
+              <p>Fame</p>
+            </div>
+            <div>
+              <p>Swagger</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <h1>Biography</h1>
+          <p>
+            Lorem ipsum dolor sit amet, conse ctetuer adipiscing elit. Aenean
+            comm odo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et ma gnis dis parturient montes, nascetur ridiculus mus.
+            Cum sociis natoque penatibus.
+          </p>
+        </div>
       </div>
       <div className={styles.right}>
-        <h1>Welcome to TraitsBox!</h1>
+        <div className={styles.top}>
+          <h1>Audio Traits</h1>
+        </div>
+        <div className={styles.bottom}>
+          <h1>Badges</h1>
+        </div>
       </div>
     </div>
   );
