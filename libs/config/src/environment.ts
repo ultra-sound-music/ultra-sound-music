deeea53fa968e06a21695340f987bda6b4144ba4
Gabriel <g@brielcastillo.me>
2022-03-10 20:35:13 -0800

    update env configs

--- libs/config/src/environment.ts
+++ libs/config/src/environment.ts
@@ -2,14 +2,16 @@
 // When building for production, this file is replaced with `environment.prod.ts`.
 import defaults from './defaults';
 
+const TRUE = 'TRUE';
+
 const env = {
-  googleAnalyticsIsEnabled: process.env.USM_GOOGLE_ANALYTICS_ENABLED,
+  googleAnalyticsIsEnabled: process.env.USM_GOOGLE_ANALYTICS_ENABLED === TRUE,
   googleAnalyticsId: process.env.USM_GOOGLE_ANALYTICS_ID,
-  sentryIsEnabled: process.env.USM_SENTRY_ENABLED,
+  sentryIsEnabled: process.env.USM_SENTRY_ENABLED === TRUE,
   sentryDNS: process.env.USM_SENTRY_DNS,
   solanaFMApiKey: process.env.USM_SOLANA_FM_API_KEY,
   solanaFMUri: process.env.USM_SOLANA_FM_URI,
-  weAreLive: process.env.WE_ARE_LIVE === 'true'
+  weAreLive: process.env.WE_ARE_LIVE === TRUE
 };
 
 const configs = {
