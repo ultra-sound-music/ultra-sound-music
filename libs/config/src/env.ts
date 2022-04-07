338aa3d8ea5c346988bfe0d3b7ac882b3a087584
Gabriel <g@brielcastillo.me>
2022-04-07 00:18:27 -0700

    fix env vars

--- libs/config/src/env.ts
+++ libs/config/src/env.ts
@@ -1,22 +1,19 @@
+export type EnvValues = string | boolean | string[];
+
 const TRUE = 'TRUE';
 
 const mplAuctionPubKeys = process.env.MPL_AUCTION_PUBKEYS
   ? process.env.MPL_AUCTION_PUBKEYS.split(' ')
-  : [];
+  : undefined;
 
 export default Object.freeze({
   googleAnalyticsIsEnabled: process.env.USM_GOOGLE_ANALYTICS_ENABLED === TRUE,
   googleAnalyticsId: process.env.USM_GOOGLE_ANALYTICS_ID,
-
   sentryIsEnabled: process.env.USM_SENTRY_ENABLED === TRUE,
   sentryDNS: process.env.USM_SENTRY_DNS,
-
   solanaFMApiKey: process.env.USM_SOLANA_FM_API_KEY,
   solanaFMUri: process.env.USM_SOLANA_FM_URI,
-
-  weAreLive:
-    process.env.WE_ARE_LIVE === TRUE && process.env.SOLANA_CLUSTER && mplAuctionPubKeys.length,
-
+  weAreLive: process.env.WE_ARE_LIVE === TRUE,
   solanaCluster: process.env.SOLANA_CLUSTER,
   mplAuctionPubKeys,
   mplStorePubKey: process.env.MPL_STORE_PUBKEY
--- libs/config/src/environment.prod.ts
+++ libs/config/src/environment.prod.ts
@@ -1,5 +1,11 @@
 export default {
   production: true,
   build: 'production',
-  environment: 'prod'
+  environment: 'prod',
+
+  mplAuctionPubKeys: [
+    'ES85FaRosnTqRaVAtYXjvCRnubehAFyjLFjP2kEyfNgx',
+    'CoLLdjSy964VTo5NdGsoAGEeuS6CSkN7T8UJPuKpaggi',
+    'Eibveh7egDAGinZ3XfNcLivVmbXFMNupJgCwzPP8hLwg'
+  ]
 };
